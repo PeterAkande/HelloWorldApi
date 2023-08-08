029ecba511eaf411c2d53a328aabc0481d1e467f
PeterAkande <akandepeter86@gmail.com>
2023-08-08 18:43:58 +0100

    Add: Add AWS Ci/Cd files needed

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -17,5 +17,5 @@ sudo apt-get update
 sudo apt-get install -y python3 python3-dev python3-pip python3-venv
 pip install --user --upgrade virtualenv
 
-# delete app
-sudo rm -rf /home/ubuntu/HelloWorldApi
+# delete app From the Directory
+sudo rm -rf /srv/HelloWorldApi
